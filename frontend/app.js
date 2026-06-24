e2e75e68ca18668548ce05c79d10ba0001672a06
Daniel Dias <danieldias@192.168.1.7>
2026-06-24 03:01:20 -0300

    fix: validação de cliente + login + ajustes finais frontend

--- frontend/app.js
+++ frontend/app.js
@@ -10,6 +10,31 @@ function auth(){
     };
 }
 
+//////////////// 🔥 LOGIN //////////////////
+
+async function login(){
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("http://127.0.0.1:8000/api/token/",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    const data = await res.json();
+
+    if(data.access){
+        localStorage.setItem("token", data.access);
+        window.location.href = "app.html";
+    }else{
+        document.getElementById("msg").innerText = "Login inválido";
+    }
+}
+
 //////////////// CLIENTES //////////////////
 
 async function carregarClientes(){
